ee73d86c3be4fbcd09abf11ec7d50b66a1c17ebf
Rohit Daftari <rohitdaftari3@gmail.com>
2021-08-04 13:01:37 +0530

    Add Jump & Stairs Climbing feature

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+
     public float speed = 12f;
-    Vector3 velocity ;
     public float gravity = -9.81f;
-    public Transform groundCheck;
     public float groundDistance = 0.4f;
-    public LayerMask layerMask;
+    public float jumpHeight = 3f;
+
     bool isGrounded;
+
+    public Transform groundCheck;
+    public LayerMask layerMask;
+    Vector3 velocity;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,16 +27,22 @@ public class PlayerMovement : MonoBehaviour
     {
 
         isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance,layerMask);
+
         if (isGrounded && velocity.y < 0) {
             velocity.y = -2f;
         }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetButton("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
 }
+
